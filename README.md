f2d46648d09bcd6da052ad4e0c3cde04f644fc6e
Rikki Gibson <rikkigibson@gmail.com>
2022-10-29 14:41:33 -0700

    Implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# FoundryVTT Condition Labels
+
+Adds a label which instantly shows the currently hovered condition in the token HUD. Faster than waiting for a tooltip.
+
+![demo](demo.gif)
---
+++ conditionLabels.js
@@ -0,0 +1,22 @@
+// FoundryVTT Condition Labels
+// Adds a label which shows the currently hovered condition in the token HUD.
+
+Hooks.on('renderTokenHUD', onRenderTokenHUD);
+
+function onRenderTokenHUD(_, $elem) {
+    const label =
+`<span style="grid-column:span 4; white-space: nowrap" id="current-status">
+    &nbsp;
+</span>`
+    const $statusEffects = $elem.find('.status-effects');
+    $statusEffects.prepend($(label));
+    $statusEffects.mouseover((e) => {
+        let title = e.target.title;
+        if (title) {
+            $("#current-status").html(title);
+        }
+    });
+    $statusEffects.mouseleave(() => {
+        $("#current-status").html('&nbsp;');
+    });
+}
